0456b6c26f6bbbbe13e67a5f443fc12cd5ffd4d2
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-07-25 16:41:35 +0530

    [Mayur] Add : Created Get users and get user by id api and tested using postman.

---
+++ FundooAPI/FundooAPI/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using FundooAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundooAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public List<User> users = new List<User>()
+        {
+            new User { Id = 1, FirstName = "Mayur", LastName = "Wankhade", Email = "mayur.wankhade2@gmail.com", PhoneNumber = "8082494818"},
+            new User { Id = 2, FirstName = "Harshal", LastName = "Kadlak", Email = "hkadlak8@gmail.com", PhoneNumber = "123456789"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+    }
+}
